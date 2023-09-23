d14a19e85e11f54fdb9327ab8cb7714d20ce3061
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2023-09-22 19:23:27 -0500

    feat: finished the generation logic

--- main.js
+++ main.js
@@ -1,9 +1,46 @@
 const CARD_WIDTH = 360;
 const CARD_HEIGHT = 280;
 
-const createCircleIcon = (slot, ctx) => {};
+const createCircleIcon = (slot, ctx) => {
+  ctx.save();
+  ctx.strokeStyle = "#fff";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.arc(100 + (slot.x - 1) * 40, 79 + (slot.y - 1) * 40, 8, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.restore();
+};
 
-const createDiamondIcon = (slot, ctx) => {};
+const createDiamondIcon = (slot, ctx) => {
+  ctx.save();
+  ctx.strokeStyle = "#fff";
+  ctx.lineWidth = 2;
+  ctx.strokeRect(92.5 + (slot.x - 1) * 40, 72 + (slot.y - 1) * 40, 15, 15);
+  ctx.restore();
+};
+
+const createXIcon = (slot, ctx) => {
+  ctx.save();
+  ctx.strokeStyle = "#fff";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(92.5 + (slot.x - 1) * 40, 72 + (slot.y - 1) * 40);
+  ctx.lineTo(107.5 + (slot.x - 1) * 40, 87 + (slot.y - 1) * 40);
+  ctx.moveTo(107.5 + (slot.x - 1) * 40, 72 + (slot.y - 1) * 40);
+  ctx.lineTo(92.5 + (slot.x - 1) * 40, 87 + (slot.y - 1) * 40);
+  ctx.stroke();
+  ctx.restore();
+};
+
+const createSkullIcon = (slot, ctx) => {
+  ctx.save();
+  ctx.strokeStyle = "#fff";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  // draw skull icon
+  ctx.stroke();
+  ctx.restore();
+};
 
 const getColor = (color) => {
   switch (color) {
@@ -19,35 +56,36 @@ const getColor = (color) => {
 };
 
 const createBlock = (color, slot, ctx) => {
+  const SIZE = 30;
+
   ctx.save();
   ctx.fillStyle = color;
   // draw rounded rect
-  ctx.beginPath();
-  ctx.moveTo(slot.x + 10, slot.y);
-  ctx.lineTo(slot.x + slot.width - 10, slot.y);
-  ctx.arcTo(slot.x + slot.width, slot.y, slot.x + slot.width, slot.y + 10, 8);
-  ctx.lineTo(slot.x + slot.width, slot.y + slot.height - 10);
-  ctx.arcTo(
-    slot.x + slot.width,
-    slot.y + slot.height,
-    slot.x + slot.width - 10,
-    slot.y + slot.height,
-    8,
-  );
-  ctx.lineTo(slot.x + 10, slot.y + slot.height);
-  ctx.arcTo(slot.x, slot.y + slot.height, slot.x, slot.y + slot.height - 10, 8);
-  ctx.lineTo(slot.x, slot.y + 10);
-  ctx.arcTo(slot.x, slot.y, slot.x + 10, slot.y, 8);
-  ctx.closePath();
+  ctx.strokeStyle = "#eee";
+  ctx.lineWidth = 1;
   ctx.fillStyle = getColor(color);
-  ctx.fill();
+  ctx.fillRect(85 + (slot.x - 1) * 40, 64 + (slot.y - 1) * 40, SIZE, SIZE);
+  ctx.strokeRect(85 + (slot.x - 1) * 40, 64 + (slot.y - 1) * 40, SIZE, SIZE);
+  if (color === "blue") {
+    createCircleIcon(slot, ctx);
+  } else if (color === "red") {
+    createDiamondIcon(slot, ctx);
+  } else if (color === "black") {
+    createXIcon(slot, ctx);
+  }
   ctx.restore();
 };
 
-(() => {
-  const cvs = document.getElementById("canvas");
-  const ctx = cvs.getContext("2d");
+const shuffle = (arr) => {
+  const newArr = [...arr];
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+  return newArr;
+};
 
+const drawBackground = (ctx) => {
   ctx.fillStyle = "#847964";
   ctx.fillRect(0, 0, 360, 280);
 
@@ -126,6 +164,76 @@ const createBlock = (color, slot, ctx) => {
   ctx.fillStyle = "#262626";
   ctx.stroke();
   ctx.fill();
+};
 
-  createBlock("red", { x: 80, y: 80, width: 40, height: 40 }, ctx);
+const createLight = (color, ctx) => {
+  ctx.save();
+  ctx.fillStyle = color === "blue" ? "#00d9ff" : "#ff001d";
+  ctx.strokeStyle = "#4f493a";
+  ctx.lineWidth = 4;
+  ctx.strokeRect(40, 110, 20, 60);
+  ctx.fillRect(40, 110, 20, 60);
+  ctx.strokeRect(CARD_WIDTH - 60, 110, 20, 60);
+  ctx.fillRect(CARD_WIDTH - 60, 110, 20, 60);
+  ctx.restore();
+};
+
+(() => {
+  const cvs = document.getElementById("canvas");
+  const ctx = cvs.getContext("2d");
+
+  drawBackground(ctx);
+
+  const seed = new URL(window.location.href).searchParams.get("seed");
+
+  let blueHasExtra;
+  let cards;
+
+  if (!seed) {
+    blueHasExtra = Math.floor(Math.random() * 2);
+    cards = shuffle([
+      ...Array(blueHasExtra ? 8 : 7).fill("blue"),
+      ...Array(blueHasExtra ? 7 : 8).fill("red"),
+      ...Array(4).fill("blank"),
+      "black",
+    ]);
+
+    const serialized = Uint8Array.from(
+      cards.map((card) => {
+        if (card === "blue") return 1;
+        if (card === "red") return 2;
+        if (card === "black") return 3;
+        return 0;
+      }),
+    );
+
+    let binary = "";
+    for (const byte of serialized) {
+      binary += String.fromCharCode(byte);
+    }
+    const base64 = btoa(binary);
+
+    window.history.pushState({}, "", `?seed=${base64}`);
+  } else {
+    cards = Array.from(Uint8Array.from(atob(seed), (c) => c.charCodeAt(0))).map(
+      (c) => {
+        if (c === 1) return "blue";
+        if (c === 2) return "red";
+        if (c === 3) return "black";
+        return "blank";
+      },
+    );
+    blueHasExtra = cards.filter((c) => c === "blue").length === 8;
+  }
+
+  cards.forEach((card, i) => {
+    const slot = {
+      x: (i % 5) + 1,
+      y: Math.floor(i / 5) + 1,
+    };
+    createBlock(card, slot, ctx);
+  });
+
+  createLight(blueHasExtra ? "blue" : "red", ctx);
+  document.getElementById("url").innerText = window.location.href;
 })();
